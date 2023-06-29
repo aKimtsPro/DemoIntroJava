acfb997a8a35c718e96520964471618ce9c2c003
Alexandre Kimtsaris <alexandre.kimtsaris@bstorm.be>
2023-06-29 09:58:37 +0200

    Correction: surcharge

--- src/be/tftic/webmobile/intro/exo/oo/classe/Main.java
+++ src/be/tftic/webmobile/intro/exo/oo/classe/Main.java
@@ -3,17 +3,12 @@ package be.tftic.webmobile.intro.exo.oo.classe;
 public class Main {
 
     public static void main(String[] args) {
-        Personne luc = new Personne();
-        luc.prenom = "luc";
-        luc.nom = "dubois";
-        luc.age = 49;
-
-        Personne marie = new Personne();
-        marie.prenom = "marie";
-        marie.nom = "dubuisson";
-        marie.age = 52;
-
+        Personne luc = new Personne("luc", "dubois", 49);
+        Personne marie = new Personne("marie", "dubuisson");
+        luc.saluer();
+        luc.saluer(true);
         luc.saluer(marie);
+        luc.saluer(marie, true);
     }
 
 }
--- src/be/tftic/webmobile/intro/exo/oo/classe/Personne.java
+++ src/be/tftic/webmobile/intro/exo/oo/classe/Personne.java
@@ -2,20 +2,52 @@ package be.tftic.webmobile.intro.exo.oo.classe;
 
 public class Personne {
 
+    // region FIELDS
     String prenom;
     String nom;
     int age;
+    // endregion
 
-    void saluer( Personne aSaluer ){
-        System.out.printf(
-                "Bonjour %s %s, je m'appelle %s %s et j'ai %d ans.\n",
-                aSaluer.prenom,
-                aSaluer.nom,
-                this.prenom,
-                this.nom,
-                this.age
-        );
+    // region CTOR
+    Personne(String prenom, String nom) {
+        this.prenom = prenom;
+        this.nom = nom;
     }
 
+    Personne(String prenom, String nom, int age) {
+        this.prenom = prenom;
+        this.nom = nom;
+        this.age = age;
+    }
+    // endregion
+
+    // region METHODS
+    void saluer(){
+        this.saluer(null, false);
+    }
+
+    void saluer(Personne aSaluer){
+        this.saluer(aSaluer, false);
+    }
+
+    void saluer(boolean sePresenter){
+        this.saluer(null, sePresenter);
+    }
+
+    void saluer(Personne aSaluer, boolean sePresenter){
+
+        String message = sePresenter ?
+                ",je suis %s %s et j'ai %d ans.".formatted(this.prenom, this.nom, this.age)
+                : "!";
+
+        if( aSaluer != null )
+            message = "Bonjour %s %s".formatted(aSaluer.prenom, aSaluer.nom) + message;
+        else
+            message = "Bonjour" + message;
+
+        System.out.println(message);
+
+    }
+    // endregion
 
 }
